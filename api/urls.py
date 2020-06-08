f081bf8ef243e4975ad11e789c2eece51fc4d8ac
Favour Anefu <favouranefu@gmail.com>
2020-06-08 14:58:08 +0100

    Added forward slash to sendemailwithtemplate

--- api/urls.py
+++ api/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('sendemail/', views.send_email),
-    path('sendemailwithtemplate', views.send_email_with_template)
+    path('sendemailwithtemplate/', views.send_email_with_template)
 ]
